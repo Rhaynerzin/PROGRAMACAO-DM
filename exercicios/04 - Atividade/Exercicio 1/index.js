88ad7e7334f51c34ff3ba9ff80b06ddad7263eae
Rhaynerzin <rhayner.mendonca@iesb.edu.br>
2025-03-12 21:53:28 -0300

    Exercicios

---
+++ exercicios/04 - Atividade/Exercicio 1/index.js
@@ -0,0 +1,5 @@
+import { soma, subtracao, multiplicacao, divisao } from './calculadora.js';
+console.log("a soma de 10+5 é: ", soma(10, 5));
+console.log("a subtração de 10-5 é: ", subtracao(10, 5));
+console.log("a multiplicação de 10*5 é: ", multiplicacao(10, 5));
+console.log("a divisão de 10/5 é: ", divisao(10, 5));
